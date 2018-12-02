4922a11cb4a621792f015a989794b53999446373
Renovamen <renovamenzxh@gmail.com>
2018-12-02 14:51:13 +0800

    update

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ const PRECACHE_LIST = [
   "./js/hux-blog.min.js",
   "./js/snackbar.js",
   "./img/icon_wechat.png",
-  "./img/avatar-hux.jpg",
+  "./img/avatar.png",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
