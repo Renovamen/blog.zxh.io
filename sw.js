98efbbd55a137c2e440d318d332ee5e8ad5ed4f4
Renovamen <renovamenzxh@gmail.com>
2020-02-15 20:24:16 +0800

    [Perf] Adjust cdn.

--- sw.js
+++ sw.js
@@ -15,24 +15,23 @@ const CACHE = CACHE_NAMESPACE + 'precache-then-runtime';
 const PRECACHE_LIST = [
   "./",
   "./offline.html",
-  "./js/library/jquery.min.js",
-  "./js/library/bootstrap.min.js",
+  "https://cdn.jsdelivr.net/npm/jquery@2.1.3/dist/jquery.min.js",
   "./js/renovamen-blog.min.js",
   "./js/snackbar.js",
   "./img/avatar.png",
   "./img/home-bg/1.jpg",
-  "./img/404-bg.jpg",
+  "./img/404.png",
   "./css/renovamen-blog.min.css",
-  "./css/library/bootstrap.css"
+  "https://cdn.jsdelivr.net/npm/bootstrap@3.3.2/dist/css/bootstrap.min.css"
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css",
   // "//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/fonts/fontawesome-webfont.woff2?v=4.6.3",
   // "//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js"
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
+  "renovamen.ink",
   "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "cdn.jsdelivr.net"
 ]
 const DEPRECATED_CACHES = ['precache-v1', 'runtime', 'main-precache-v1', 'main-runtime']
 
